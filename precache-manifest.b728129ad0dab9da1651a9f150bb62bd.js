d97e234b78e58df12b4ad26c6924298544d95c40
Etherum7 <hvs.231918@gmail.com>
2020-06-11 18:47:11 +0530

    Updates

---
+++ precache-manifest.b728129ad0dab9da1651a9f150bb62bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c7f0a9b1e3d5381bfe7e9d038a7e6e2",
+    "url": "/pick-your-dream-color/index.html"
+  },
+  {
+    "revision": "807ca3ffd11fcd8b5c2d",
+    "url": "/pick-your-dream-color/static/css/2.0da342d8.chunk.css"
+  },
+  {
+    "revision": "38914a100b17ef09614e",
+    "url": "/pick-your-dream-color/static/css/main.5f9fe60e.chunk.css"
+  },
+  {
+    "revision": "807ca3ffd11fcd8b5c2d",
+    "url": "/pick-your-dream-color/static/js/2.a6cf9f02.chunk.js"
+  },
+  {
+    "revision": "fe2f19c5d5965d51615a596cf8d07fe9",
+    "url": "/pick-your-dream-color/static/js/2.a6cf9f02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38914a100b17ef09614e",
+    "url": "/pick-your-dream-color/static/js/main.5f93e51c.chunk.js"
+  },
+  {
+    "revision": "4324bb5069ee92ee0427",
+    "url": "/pick-your-dream-color/static/js/runtime-main.74212797.js"
+  }
+]);
